651bb3aba2208695e723fcf210d91602156295de
hayesall <alexander@batflyer.net>
2022-06-05 14:12:35 -0400

    ✅ Add specific number of trees in unit test

--- src/test/java/edu/wisc/cs/will/RunBoostedModelsTest.java
+++ src/test/java/edu/wisc/cs/will/RunBoostedModelsTest.java
@@ -9,9 +9,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RunBoostedModelsTest
 {
     @Test
-    public void testFoo()
-    {
-        String[] trainArgs = {"-l", "-train", "data/Toy-Cancer/train/", "-target", "cancer"};
+    public void testToyCancerLearnInfer() {
+
+        String[] trainArgs = {"-l", "-train", "data/Toy-Cancer/train/", "-target", "cancer", "-trees", "10"};
         RunBoostedModels.main(trainArgs);
 
         String[] testArgs = {"-i", "-model", "data/Toy-Cancer/train/models/", "-test", "data/Toy-Cancer/test/", "-target", "cancer"};
