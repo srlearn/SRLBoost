027b868633f7fcb09096bf4372332a6510d1fce7
hayesall <alexander@batflyer.net>
2022-06-11 16:29:39 -0400

    🔥 Drop unused methods/constructors in `ProbDistribution`

--- src/main/java/edu/wisc/cs/will/Utils/ProbDistribution.java
+++ src/main/java/edu/wisc/cs/will/Utils/ProbDistribution.java
@@ -15,10 +15,6 @@ public class ProbDistribution {
 	private double[] probDistribution = null;
 	
 	private boolean hasDistribution;
-	
-	public ProbDistribution(double prob) {
-		setProbOfBeingTrue(prob);
-	}
 
 	public ProbDistribution(double prob, boolean regression) {
 		setProbOfBeingTrue(prob, regression);
@@ -31,15 +27,6 @@ public class ProbDistribution {
 		}
 	}
 
-	public ProbDistribution(ProbDistribution copy) {
-		this.hasDistribution  = copy.hasDistribution;
-		if (hasDistribution) {
-			this.probDistribution = copy.probDistribution.clone();
-		} else {
-			this.probOfBeingTrue = copy.probOfBeingTrue;
-		}
-	}
-	
 	/*
 	 * Construct distribution using sigmoid
 	 */
@@ -78,25 +65,6 @@ public class ProbDistribution {
 			setProbDistribution(probDist);
 		} else {
 			setProbOfBeingTrue(BoostingUtils.sigmoid(reg.getSingleRegressionValue(), 0));
-		}
-	}
-	public void scaleDistribution(double scalar) {
-		if (isHasDistribution()) {
-			probDistribution = VectorStatistics.scalarProduct(probDistribution, scalar); 
-		} else {
-			probOfBeingTrue *= scalar;
-		}
-	}
-	
-	public void addDistribution(ProbDistribution add){
-		// If null, then add 0
-		if (add == null) {
-			return;
-		}
-		if (isHasDistribution()) {
-			probDistribution = VectorStatistics.addVectors(this.probDistribution, add.probDistribution);
-		} else {
-			probOfBeingTrue += add.probOfBeingTrue;
 		}
 	}
 
@@ -142,32 +110,6 @@ public class ProbDistribution {
 
 	private void setHasDistribution(boolean hasDistribution) {
 		this.hasDistribution = hasDistribution;
-	}
-
-	public double norm() {
-		if (isHasDistribution()) {
-			return Math.sqrt(VectorStatistics.dotProduct(probDistribution, probDistribution));
-		}
-		return probOfBeingTrue;
-	}
-
-	/*
-	 * Return a randomly selected value from the distribution.
-	 */
-	public int randomlySelect() {
-		if (!isHasDistribution()) {
-			return (Utils.random() < probOfBeingTrue) ? 1 : 0;
-		}
-		double cumulative = 0;
-		double rand = Utils.random();
-		for (int i = 0; i < probDistribution.length; i++) {
-			cumulative += probDistribution[i];
-			if (rand < cumulative) {
-				return i;
-			}
-		}
-		Utils.error("Cumulative distribution doesn't sum to 1. Sum:" + cumulative);
-		return 0;
 	}
 
 }
